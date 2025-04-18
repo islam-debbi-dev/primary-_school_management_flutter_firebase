ec1443aeadeed4266a41c612cfbc47fb8083b815
islam_debbi <islamdebbi6@gmail.com>
2025-04-18 15:04:52 +0200

    - make it as clean architecture split folders

--- lib/features/admin/presentation/home_admin.dart
+++ lib/features/admin/presentation/home_admin.dart
@@ -111,6 +111,7 @@ class _homeadminState extends State<HomeAdmin> {
                     Navigator.pushNamed(
                         context, MessagesForStudents.screenroute);
                   },
+      
                 ),
               ),
               const SizedBox(
